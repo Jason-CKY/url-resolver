f6e249f022f2460b1334319ea2ec6505693a7ee1
Jason Cheng <jasoncky96@gmail.com>
2024-07-17 16:49:33 +0800

    added healthcheck and tests

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/mroth/weightedrand/v2"
 	log "github.com/sirupsen/logrus"
@@ -16,16 +20,21 @@ import (
 
 var (
 	// Config
-	webPort         int    = 8080
-	routingFilePath string = "routing.json"
-	doHealthCheck   bool   = false
+	webPort                   int    = 8080
+	routingFilePath           string = "routing.json"
+	doHealthCheck             bool   = false
+	healthCheckTimeoutSeconds int    = 5
 )
 
-type RoutingRule map[string][]Upstream
-
 type Upstream struct {
-	Url    string `json:"url"`
-	Weight int    `json:"weight"`
+	Url                 string  `json:"url"`
+	Weight              int     `json:"weight"`
+	HealthcheckEndpoint *string `json:"healthcheck_endpoint"`
+	Healthy             bool    `json:"ready"`
+}
+
+type RoutingRule struct {
+	Upstreams []Upstream `json:"upstreams"`
 }
 
 func LookupEnvOrString(key string, defaultValue string) string {
@@ -56,7 +65,7 @@ func LookupEnvOrBool(key string, defaultValue bool) bool {
 	return strings.ToLower(envVariable) == "true"
 }
 
-func readConfig(fpath string) map[string]*weightedrand.Chooser[string, int] {
+func ReadConfigRules(fpath string) map[string]RoutingRule {
 	file, err := os.Open(fpath)
 	if err != nil {
 		log.Errorf("%v", err)
@@ -68,15 +77,20 @@ func readConfig(fpath string) map[string]*weightedrand.Chooser[string, int] {
 	}
 	var routingRules map[string]RoutingRule
 	json.Unmarshal(byteValue, &routingRules)
+	return routingRules
+}
 
+func GetRandomChooser(routingRules map[string]RoutingRule, doHealthCheck bool) map[string]*weightedrand.Chooser[string, int] {
 	weightedChoosers := map[string]*weightedrand.Chooser[string, int]{}
 
 	for prefix, routingRule := range routingRules {
 		var weightedChoices []weightedrand.Choice[string, int]
 
-		for i := 0; i < len(routingRule["upstreams"]); i++ {
-			upstream := routingRule["upstreams"][i]
-			weightedChoices = append(weightedChoices, weightedrand.NewChoice(upstream.Url, upstream.Weight))
+		for i := 0; i < len(routingRule.Upstreams); i++ {
+			upstream := routingRule.Upstreams[i]
+			if (upstream.HealthcheckEndpoint != nil && upstream.Healthy) || !doHealthCheck {
+				weightedChoices = append(weightedChoices, weightedrand.NewChoice(upstream.Url, upstream.Weight))
+			}
 		}
 		weightedChoosers[prefix], _ = weightedrand.NewChooser(weightedChoices...)
 	}
@@ -84,18 +98,66 @@ func readConfig(fpath string) map[string]*weightedrand.Chooser[string, int] {
 	return weightedChoosers
 }
 
-func resolveUrl(prefix string, weightedChoosers map[string]*weightedrand.Chooser[string, int]) (route string, found bool) {
+func ResolveUrl(prefix string, weightedChoosers map[string]*weightedrand.Chooser[string, int]) (route string, found bool, err error) {
 	chooser, ok := weightedChoosers[prefix]
 	if !ok {
-		return "", ok
+		return "", ok, nil
 	}
-	return chooser.Pick(), ok
+	if chooser == nil {
+		return "", ok, errors.New("no healthy upstream found")
+	}
+	return chooser.Pick(), ok, nil
+}
+
+func HealthCheck(healthCheckEndpoint string) bool {
+	req, httpErr := http.NewRequest(http.MethodGet, healthCheckEndpoint, nil)
+	if httpErr != nil {
+		return false
+	}
+	client := &http.Client{
+		Timeout: time.Duration(healthCheckTimeoutSeconds) * time.Second,
+	}
+	res, httpErr := client.Do(req)
+	if httpErr != nil {
+		return false
+	}
+	return res.StatusCode == 200
+}
+
+func HealthCheckAll(routingRules map[string]RoutingRule) {
+	for prefix := range routingRules {
+		for i := 0; i < len(routingRules[prefix].Upstreams); i++ {
+			if routingRules[prefix].Upstreams[i].HealthcheckEndpoint != nil {
+				routingRules[prefix].Upstreams[i].Healthy = HealthCheck(*routingRules[prefix].Upstreams[i].HealthcheckEndpoint)
+			}
+		}
+	}
+}
+
+func ValidateHealthCheck(routingRules map[string]RoutingRule) error {
+	// if healthcheck is turned on, make sure config has health check endpoint for all route upstreams
+	for prefix := range routingRules {
+		for i := 0; i < len(routingRules[prefix].Upstreams); i++ {
+			if routingRules[prefix].Upstreams[i].HealthcheckEndpoint == nil {
+				return errors.New("healthcheck endpoint does not exist in config file")
+			}
+		}
+	}
+	return nil
 }
 
 func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Infof("Error loading .env file: %v\nUsing environment variables instead...", err)
+	}
+
 	flag.StringVar(&routingFilePath, "fpath", LookupEnvOrString("CONFIG_FPATH", routingFilePath), "Path to routing json file")
 	flag.IntVar(&webPort, "port", LookupEnvOrInt("PORT", webPort), "Port for echo web server")
 	flag.BoolVar(&doHealthCheck, "healthcheck", LookupEnvOrBool("HEALTHCHECK", doHealthCheck), "Whether to do healthchecks for updating routing rules. Will not route to endpoints that are not ready")
+	flag.IntVar(&healthCheckTimeoutSeconds, "healthcheck-timeout-seconds", LookupEnvOrInt("HEALTHCEHCK_TIMEOUT_SECONDS", healthCheckTimeoutSeconds), "Number of timeout seconds for healthcheck")
 	flag.Parse()
 
 	// setup logrus
@@ -105,17 +167,35 @@ func main() {
 	})
 
 	log.Infof("Reading routing file at %s", routingFilePath)
-	weightedChoosers := readConfig(routingFilePath)
+	routingRules := ReadConfigRules(routingFilePath)
+
+	if doHealthCheck {
+		err = ValidateHealthCheck(routingRules)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		go func() {
+			HealthCheckAll(routingRules)
+			time.Sleep(5 * time.Second)
+		}()
+	}
 
 	e := echo.New()
+	e.GET("/health", func(c echo.Context) error {
+		return c.String(200, "Healthy")
+	})
 	e.GET("/:prefix", func(c echo.Context) error {
 		prefix := "/" + c.Param("prefix")
-		route, found := resolveUrl(prefix, weightedChoosers)
+		weightedChoosers := GetRandomChooser(routingRules, doHealthCheck)
+		route, found, err := ResolveUrl(prefix, weightedChoosers)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]string{"detail": err.Error()})
+		}
 		if found {
-			return c.JSON(200, map[string]string{"route": route})
+			return c.JSON(http.StatusOK, map[string]string{"route": route})
 		} else {
-			log.Errorf("Prefix not found in routing rules")
-			return c.JSON(404, map[string]string{"detail": fmt.Sprintf("%s does not exist in the routing rules", prefix)})
+			return c.JSON(http.StatusNotFound, map[string]string{"detail": fmt.Sprintf("%s does not exist in the routing rules", prefix)})
 		}
 
 	})
--- main_test.go
+++ main_test.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"math"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRouteNotFound(t *testing.T) {
 	prefix := "/not-found"
-	weightedChoosers := readConfig(routingFilePath)
-	_, found := resolveUrl(prefix, weightedChoosers)
+	routingRules := ReadConfigRules(routingFilePath)
+	weightedChoosers := GetRandomChooser(routingRules, false)
+	_, found, err := ResolveUrl(prefix, weightedChoosers)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
 	if found {
 		t.Fatalf(`prefix %v not found in config file`, prefix)
 	}
@@ -17,13 +29,17 @@ func TestRouteNotFound(t *testing.T) {
 func TestEqualWeight(t *testing.T) {
 	numIterations := 10000
 	prefix := "/test1"
-	weightedChoosers := readConfig(routingFilePath)
+	routingRules := ReadConfigRules(routingFilePath)
+	weightedChoosers := GetRandomChooser(routingRules, false)
 	counts := make(map[string]int, len(weightedChoosers))
 	for k := range weightedChoosers {
 		counts[k] = 0
 	}
 	for i := 0; i < numIterations; i++ {
-		route, found := resolveUrl(prefix, weightedChoosers)
+		route, found, err := ResolveUrl(prefix, weightedChoosers)
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
 		if !found {
 			t.Fatalf(`prefix %v not found in config file`, prefix)
 		}
@@ -38,13 +54,17 @@ func TestEqualWeight(t *testing.T) {
 func TestDifferentWeight(t *testing.T) {
 	numIterations := 10000
 	prefix := "/test2"
-	weightedChoosers := readConfig(routingFilePath)
+	routingRules := ReadConfigRules(routingFilePath)
+	weightedChoosers := GetRandomChooser(routingRules, false)
 	counts := make(map[string]int, len(weightedChoosers))
 	for k := range weightedChoosers {
 		counts[k] = 0
 	}
 	for i := 0; i < numIterations; i++ {
-		route, found := resolveUrl(prefix, weightedChoosers)
+		route, found, err := ResolveUrl(prefix, weightedChoosers)
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
 		if !found {
 			t.Fatalf(`prefix %v not found in config file`, prefix)
 		}
@@ -53,5 +73,68 @@ func TestDifferentWeight(t *testing.T) {
 	ratio := float64(counts["http://10.20.10.10"]) / float64(counts["https://test.example.site"])
 	if math.Abs(ratio-1.0/20.0) > 0.1 {
 		t.Fatalf(`ratio %v off by more than 0.1`, ratio)
+	}
+}
+
+func TestValidateHealthCheck(t *testing.T) {
+	routingRules := ReadConfigRules(routingFilePath)
+	var noHealthCheckUpstreams []Upstream
+	noHealthCheckUpstreams = append(noHealthCheckUpstreams, Upstream{Url: "http://test", HealthcheckEndpoint: nil})
+	routingRules["error"] = RoutingRule{
+		Upstreams: noHealthCheckUpstreams,
+	}
+	err := ValidateHealthCheck(routingRules)
+	if err == nil {
+		t.Fatalf("failed to raise error")
+	}
+}
+
+func TestRoutingWithHealthCheck(t *testing.T) {
+	routingRules := ReadConfigRules(routingFilePath)
+	routingRules["/test1"].Upstreams[0].Healthy = true
+	// Create a new Echo instance
+	e := echo.New()
+	// test1 should always return
+	// Define the request
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/:prefix")
+	c.SetParamNames("prefix")
+	c.SetParamValues("test1")
+	// Call the handler function
+	routeHandler := func(c echo.Context) error {
+		prefix := "/" + c.Param("prefix")
+		weightedChoosers := GetRandomChooser(routingRules, true)
+		route, found, err := ResolveUrl(prefix, weightedChoosers)
+		if err != nil {
+			return c.JSON(400, map[string]string{"detail": err.Error()})
+		}
+		if found {
+			return c.JSON(200, map[string]string{"route": route})
+		} else {
+			return c.JSON(404, map[string]string{"detail": fmt.Sprintf("%s does not exist in the routing rules", prefix)})
+		}
+	}
+
+	if assert.NoError(t, routeHandler(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		expectedResponse := map[string]string{"route": "http://10.20.10.10"}
+		expectedResponseString, _ := json.Marshal(expectedResponse)
+		assert.Equal(t, string(expectedResponseString), strings.TrimSuffix(rec.Body.String(), "\n"))
+	}
+
+	req = httptest.NewRequest(http.MethodGet, "/", nil)
+	rec = httptest.NewRecorder()
+	c = e.NewContext(req, rec)
+	c.SetPath("/:prefix")
+	c.SetParamNames("prefix")
+	c.SetParamValues("test2")
+
+	if assert.NoError(t, routeHandler(c)) {
+		assert.Equal(t, http.StatusBadRequest, rec.Code)
+		expectedResponse := map[string]string{"detail": "no healthy upstream found"}
+		expectedResponseString, _ := json.Marshal(expectedResponse)
+		assert.Equal(t, string(expectedResponseString), strings.TrimSuffix(rec.Body.String(), "\n"))
 	}
 }
